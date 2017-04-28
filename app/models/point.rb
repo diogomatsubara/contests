070fa843a37803b4318dc8b6377fc404437da225
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-28 07:43:33 -0300

    Solution to spec/customtype_spec.rb -e rq03: add Point.demongoize method

--- app/models/point.rb
+++ app/models/point.rb
@@ -10,4 +10,8 @@ class Point
   def mongoize
     {:type=>"Point", :coordinates=>[@longitude, @latitude]}
   end
+
+  def self.demongoize(point_hash)
+    Point.new(point_hash[:coordinates][0], point_hash[:coordinates][1])
+  end
 end
