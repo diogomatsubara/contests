896f13638ff5cb80973bf8dd60ce9e8fcb693291
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-28 16:22:33 -0300

    solution to spec/lecture6_spec.rb -e rq04: Add restriction to delete Venue if it has a reference to Contest, remove unnecessary _id field from Entrant

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -1,6 +1,5 @@
 class Entrant
   include Mongoid::Document
-  field :_id, type: Integer
   field :name, type: String
   field :group, type: String
   field :secs, type: Float
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,5 +3,5 @@ class Venue
   field :name, type: String
 
   embeds_one :address, as: :addressable, class_name: "Address"
-  has_many :contests
+  has_many :contests, dependent: :restrict
 end
