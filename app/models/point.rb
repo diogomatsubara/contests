76ce933b7f1c3f5b060990a11c8764251925cb1c
Diogo Matsubara <diogo.matsubara@gmail.com>
2017-04-28 08:00:19 -0300

    Solution to spec/customtype_spec.rb -e rq04: add Point class method mongoize

--- app/models/point.rb
+++ app/models/point.rb
@@ -11,7 +11,21 @@ class Point
     {:type=>"Point", :coordinates=>[@longitude, @latitude]}
   end
 
-  def self.demongoize(point_hash)
-    Point.new(point_hash[:coordinates][0], point_hash[:coordinates][1])
+  def self.demongoize(object)
+    Point.new(object[:coordinates][0], object[:coordinates][1])
+  end
+
+  def self.mongoize(object)
+    case object
+    when Point then object.mongoize
+    when Hash then
+      if object[:type]
+        Point.new(object[:coordinates][0], object[:coordinates][1]).mongoize
+      else
+        Point.new(object[:lng], object[:lat]).mongoize
+      end
+    else object
+    end
+
   end
 end
